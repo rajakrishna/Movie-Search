f0b74be206645b62b14b6e854f601242db799ded
Raja Krishna <rajakrishna214@gmail.com>
2020-03-30 22:35:38 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "../App.css";
 import Header from "./Header";
 import Movie from "./Movie";
-import Search from "./Search";
 
 // Here is your key: 986aab81
 
