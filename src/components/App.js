a248d5c205825855b2ad9a05cf737398f81f1daf
Raja Krishna <rajakrishna214@gmail.com>
2020-03-30 21:46:09 -0500

    updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
 
-function App() {
+// Here is your key: 986aab81
+
+// Please append it to all of your API requests,
+
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=986aab81
+
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=986aab81"; // you should replace this with yours
+
+const App = () => {
+	const [loading, setLoading] = useState(true);
+	const [movies, setMovies] = useState([]);
+	const [errorMessage, setErrorMessage] = useState(null);
+
+	useEffect(() => {
+		fetch(MOVIE_API_URL)
+			.then(response => response.json())
+			.then(jsonResponse => {
+				setMovies(jsonResponse.Search);
+				setLoading(false);
+			});
+	}, []);
+
+	const search = searchValue => {
+		setLoading(true);
+		setErrorMessage(null);
+
+		fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=986aab81`)
+			.then(response => response.json())
+			.then(jsonResponse => {
+				if (jsonResponse.Response === "True") {
+					setMovies(jsonResponse.Search);
+					setLoading(false);
+				} else {
+					setErrorMessage(jsonResponse.Error);
+					setLoading(false);
+				}
+			});
+	};
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<h1>Hellow</h1>
-			</header>
+			<Header text="Movie Search" />
+			<Search search={search} />
+			<p className="App-intro">Sharing a few of our favourite movies</p>
+			<div className="movies">
+				{loading && !errorMessage ? (
+					<span>loading...</span>
+				) : errorMessage ? (
+					<div className="errorMessage">{errorMessage}</div>
+				) : (
+					movies.map((movie, index) => (
+						<Movie key={`${index}-${movie.Title}`} movie={movie} />
+					))
+				)}
+			</div>
 		</div>
 	);
-}
+};
 
 export default App;
